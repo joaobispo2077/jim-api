3e109c0f74aa2c7e6808702a2e20134a14947c2a
João Bispo <joaobispo2077@gmail.com>
2023-05-21 21:42:00 -0300

    feat: add linters

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config';
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const configSchema = z.object({
-    PORT: z.coerce.number().default(3333),
-    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
 })
 
-const _configs  = configSchema.safeParse(process.env);
+const _configs = configSchema.safeParse(process.env)
 
 if (!_configs.success) {
   console.error(`Invalid environment variables`, _configs.error.format())
