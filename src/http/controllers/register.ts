9bb8302d23025a7e60432406956e1ddbc1a2f3d0
João Bispo <joaobispo2077@gmail.com>
2023-05-28 11:39:47 -0300

    feat: add create user controller

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@src/lib/prisma'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const userBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+
+  const { name, email, password } = userBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
