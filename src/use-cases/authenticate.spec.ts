228deb5ce9de80b28d2d8f9ff1179dbf92991af1
João Bispo <joaobispo2077@gmail.com>
2023-06-03 11:45:43 -0300

    test: should not authenticate with wrong credentials

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from 'vitest'
 import { InMemoryUsersRepository } from '@src/repositories/in-memory/in-memory-users-repository'
-import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
 import { configs } from '@src/configs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate User Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -25,5 +25,47 @@ describe('Authenticate User Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    const email = 'john.doe@gmail.com'
+    const password = '123456'
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email,
+      password_hash: await hash(password, configs.SALT_ROUND),
+    })
+
+    await expect(
+      authenticateUseCase.execute({
+        email: 'wrong-email',
+        password,
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    const email = 'john.doe@gmail.com'
+    const password = '123456'
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email,
+      password_hash: await hash(password, configs.SALT_ROUND),
+    })
+
+    await expect(
+      authenticateUseCase.execute({
+        email,
+        password: 'abcdefgh',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
