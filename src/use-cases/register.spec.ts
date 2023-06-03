0aaf131a1f00829641c09de7c9f89d365cf1d550
João Bispo <joaobispo2077@gmail.com>
2023-06-03 10:54:57 -0300

    test: shoud not be able to register with same email

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@src/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
@@ -14,11 +15,32 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    const isPasswordCorrectlyHashsed = await compare(
+    const isPasswordCorrectlyHashed = await compare(
       '123456',
       user.password_hash,
     )
 
-    expect(isPasswordCorrectlyHashsed).toBeTruthy()
+    expect(isPasswordCorrectlyHashed).toBeTruthy()
+  })
+
+  it('shoud not be able to register with same email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'john.doe@gmail.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
