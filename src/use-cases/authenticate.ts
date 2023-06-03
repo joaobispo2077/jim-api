9093e8919cc0f839c8dace7fb71f203603220e3f
João Bispo <joaobispo2077@gmail.com>
2023-06-03 11:36:14 -0300

    feat: add AuthenticateUseCase

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,37 @@
+import { UsersRepository } from '@src/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+export type AuthenticateUseCaseRequest = {
+  email: string
+  password: string
+}
+export type AuthenticateUseCaseResponse = {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
