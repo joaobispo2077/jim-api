9fe4f13240aa09ceea4678d24b17bbb37a5c87ea
João Bispo <joaobispo2077@gmail.com>
2023-06-03 12:23:30 -0300

    feat: add find by id

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -3,6 +3,14 @@ import { prisma } from '@src/lib/prisma'
 import { UsersRepository } from './users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    return await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const isUserAlreadyExists = await prisma.user.findUnique({
       where: {
