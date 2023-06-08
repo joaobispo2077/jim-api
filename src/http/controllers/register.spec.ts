eae8ce866b88369020734ece8c97d71e703826c9
João Bispo <joaobispo2077@gmail.com>
2023-06-08 15:06:33 -0300

    test(e2e): should be able to register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@src/app'
 
 describe('Register User Controller', () => {
-  it('shoud be able to register', async () => {
-    expect(1).toEqual(1)
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '12345678',
+    })
+
+    expect(response.status).toBe(201)
   })
 })
