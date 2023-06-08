8d037098c53e30b66551dc1c6a9fde680555b68f
João Bispo <joaobispo2077@gmail.com>
2023-06-08 15:09:59 -0300

    test(e2e): should be able to authenticate

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,34 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@src/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    const email = 'john.doe@gmail.com'
+    const password = '12345678'
+    const responseCreated = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email,
+      password,
+    })
+
+    expect(responseCreated.status).toBe(201)
+
+    const response = await request(app.server).post('/sessions').send({
+      email,
+      password,
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('token')
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+})
