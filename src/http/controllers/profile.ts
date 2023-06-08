9fa1d943e2241f69e5aeea8899bc0cc6dd4572f9
João Bispo <joaobispo2077@gmail.com>
2023-06-08 13:05:06 -0300

    feat: verify jwt before run /me

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,9 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify()
+
+  const userId = request.user.sub
+
+  return reply.status(200).send()
+}
